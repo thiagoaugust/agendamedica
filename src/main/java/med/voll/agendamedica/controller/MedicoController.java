95e1b7b33851e46c4967d67f454881e599b2b166
Thiago Rasquino <thiago.programador.net@gmail.com>
2022-11-21 09:09:54 -0300

    Listagem de clientes e médicos

--- src/main/java/med/voll/agendamedica/controller/MedicoController.java
+++ src/main/java/med/voll/agendamedica/controller/MedicoController.java
@@ -12,8 +12,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
--- src/main/java/med/voll/agendamedica/controller/PacienteController.java
+++ src/main/java/med/voll/agendamedica/controller/PacienteController.java
@@ -1,14 +1,15 @@
 package med.voll.agendamedica.controller;
 
 import jakarta.validation.Valid;
+import med.voll.agendamedica.paciente.DadosListagemPaciente;
 import med.voll.agendamedica.paciente.DadosPaciente;
 import med.voll.agendamedica.paciente.Paciente;
 import med.voll.agendamedica.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -19,7 +20,11 @@ public class PacienteController {
 
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosPaciente dados){
-
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
